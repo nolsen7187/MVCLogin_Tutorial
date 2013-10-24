2888d84e14a29a3a2a999b16376c0c0e7938f075
nolsen7187 <nolsen@regis.edu>
2013-10-23 20:59:04 -0600

    MVC Login Application Foundation

---
+++ SimpleLogInSystem/SimpleLogInSystem/MainDbModel.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'C:\Users\NOLSEN\Source\Repos\SimpleLogInSystem\SimpleLogInSystem\MainDbModel.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
---
+++ SimpleLogInSystem/SimpleLogInSystem/Models/UserModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SimpleLogInSystem.Models
+{
+    public class UserModel
+    {
+        [Required]
+        [EmailAddress]
+        [StringLength(150)]
+        public string email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(20, MinimumLength=6)]
+        public string password { get; set; }
+    }
+}
